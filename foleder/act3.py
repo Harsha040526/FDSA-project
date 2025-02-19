5c1361de236f6afd7ac71c27538e83402f163fbf
Harsha040526 <harshambca24@rvu.edu.in>
2025-02-19 11:04:59 +0530

    act3.py

--- foleder/act3.py
+++ foleder/act3.py
@@ -1,1 +1,14 @@
+def rec(n):
+    if n == 0:
+        return 0
+    else:
+        remainder = n % 10  
+        quotient = n // 10 
+        
+       
+        print(f"Number: {n}, Quotient: {quotient}, Remainder: {remainder}")
+        
+        return remainder + rec(quotient)  
 
+n = int(input("Enter a number: "))
+print("Sum of remainders:", rec(n))
